642063763bebce0756d89b8a0ac0e22636069311
Joe Yates <joe.g.yates@gmail.com>
2009-10-12 07:55:36 +0200

    Restore previous events when disactivating.

--- draggable.js
+++ draggable.js
@@ -1,15 +1,18 @@
 // Depends on primitives, js-object-additions
 Canvas.Draggable = function(ctx, options) {
-  var posPreDragOfset = null;
-  var posDragOrigin = null;
-
   var self = this;
+
+  var posPreDragOffset = null;
+  var posDragOrigin = null;
+  // Restore events on mouseup
+  var originalMouseMove = null;
+  var originalMouseUp = null;
 
   var mousemove = function(evt) {
     if(posDragOrigin == null)
       return true;
-    var nNewX = posPreDragOfset.x + evt.clientX - posDragOrigin.x;
-    var nNewY = posPreDragOfset.y + evt.clientY - posDragOrigin.y;
+    var nNewX = posPreDragOffset.x + evt.clientX - posDragOrigin.x;
+    var nNewY = posPreDragOffset.y + evt.clientY - posDragOrigin.y;
     ctx.setTranslation(p(nNewX, nNewY));
     if(options.onmousemove)
       options.onmousemove();
@@ -18,15 +21,17 @@ Canvas.Draggable = function(ctx, options) {
 
   var mouseup = function(evt) {
     posDragOrigin = null;
-    posPreDragOfset = null;
-    ctx.canvas.onmousemove = null;
-    ctx.canvas.onmouseup = null;
+    posPreDragOffset = null;
+    ctx.canvas.onmousemove = originalMouseMove;
+    ctx.canvas.onmouseup = originalMouseUp;
     return true;
   };
 
   this.capture = function(evt) {
     posDragOrigin = p(evt.clientX, evt.clientY);
-    posPreDragOfset = ctx.translation.clone();
+    posPreDragOffset = ctx.translation.clone();
+    originalMouseMove = ctx.canvas.onmousemove;
+    originalMouseUp = ctx.canvas.onmouseup;
     ctx.canvas.onmousemove = mousemove;
     ctx.canvas.onmouseup = mouseup;
   };
